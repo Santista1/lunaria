1a436440165e6903d66102cee6519233e721f101
Santista1 <santista1@proton.me>
2023-06-01 20:53:12 -0300

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import { Starfield } from "./components/Starfield.jsx"
 import { Moon } from "./components/Moon.jsx"
 import { Painting } from "./components/Painting.jsx"
 import { Button } from "./components/Button.jsx"
-import { Wallet } from "./components/Wallet.jsx"
 
 export default function App() {
   const view = useRef()
@@ -22,6 +21,7 @@ export default function App() {
 
   return (
     <>
+      <div className='dot' />
       <Canvas shadows>
         <Hud renderPriority={2}>
           <OrbitControls />
@@ -40,11 +40,9 @@ export default function App() {
         <PointerLockControls />
 
         <Starfield factor={3} />
-
         <Moon scale={2} position={[10, 20, -100]} />
 
         <Room scale={[6, 3, 60]} />
-
         <Bench position={[2.5, -1, 0]} />
 
         <pointLight castShadow distance={15} color='blue' position={[0, 0, -30]} intensity={0.4} />
@@ -53,48 +51,56 @@ export default function App() {
         <pointLight castShadow distance={15} position={[0, 0, 15]} intensity={0.4} />
         <pointLight castShadow distance={15} color='blue' position={[0, 0, 30]} intensity={0.4} />
 
-        <pointLight castShadow distance={1} position={[-2.5, 0, 0]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, 2]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, 4]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, -2]} intensity={5} />
+        <group position={[0, 0, 0]}>
+          <pointLight distance={0.95} position={[-2.5, 0, 3]} intensity={5} />
+          <pointLight distance={0.95} position={[-2.5, 0, 1]} intensity={5} />
+          <pointLight distance={0.95} position={[-2.5, 0, -1]} intensity={5} />
+          <pointLight distance={0.95} position={[-2.5, 0, -3]} intensity={5} />
 
-        <pointLight castShadow distance={1} position={[-2.5, 0, -10]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, -12]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, -14]} intensity={5} />
-        <pointLight castShadow distance={1} position={[-2.5, 0, -16]} intensity={5} />
+          <Painting
+            color='hotpink'
+            position={[-2.95, 0, 3]}
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239383/5B758959-5682-4A83-AA20-A429C53AF854_exp9da.jpg'
+          />
+          <Painting
+            color='hotpink'
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/IMG_5638_vqwa7h.jpg'
+            position={[-2.95, 0, 1]}
+          />
+          <Painting
+            color='hotpink'
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239389/IMG_5667_fhvrbt.jpg'
+            position={[-2.95, 0, -1]}
+          />
+          <Painting
+            color='hotpink'
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/AACABEAC-F96C-46D9-8AC5-83C5994C7F4E_grnioz.jpg'
+            position={[-2.95, 0, -3]}
+          />
+        </group>
 
-        <Painting
-          position={[-2.9, 0, 4]}
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239383/5B758959-5682-4A83-AA20-A429C53AF854_exp9da.jpg'
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/IMG_5638_vqwa7h.jpg'
-          position={[-2.9, 0, 2]}
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239389/IMG_5667_fhvrbt.jpg'
-          position={[-2.9, 0, 0]}
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/AACABEAC-F96C-46D9-8AC5-83C5994C7F4E_grnioz.jpg'
-          position={[-2.9, 0, -2]}
-        />
-        <Painting
-          position={[-2.9, 0, -10]}
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1685218397/FjLR2_eXEAAbI2J_os2zlu.jpg'
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1685218541/FgKEctFWIAAyFFc_fudqvi.jpg'
-          position={[-2.9, 0, -12]}
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1685221244/782ed0d298ebfffca45a5d6a91a4a167_tnmbyn.png'
-          position={[-2.9, 0, -14]}
-        />
-        <Painting
-          map='https://res.cloudinary.com/dexin8o58/image/upload/v1685221048/three_j414x5.gif'
-          position={[-2.9, 0, -16]}
-        />
+        <group position={[0, 0, -15]}>
+          <pointLight castShadow distance={1} position={[-2.5, 0, 3]} intensity={2} />
+          <pointLight castShadow distance={1} position={[-2.5, 0, 1]} intensity={2} />
+          <pointLight castShadow distance={1} position={[-2.5, 0, -1]} intensity={2} />
+          <pointLight castShadow distance={1} position={[-2.5, 0, -3]} intensity={2} />
+          <Painting
+            position={[-2.95, 0, 3]}
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1685218397/FjLR2_eXEAAbI2J_os2zlu.jpg'
+          />
+          <Painting
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1685218541/FgKEctFWIAAyFFc_fudqvi.jpg'
+            position={[-2.95, 0, 1]}
+          />
+          <Painting
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1685221244/782ed0d298ebfffca45a5d6a91a4a167_tnmbyn.png'
+            position={[-2.95, 0, -1]}
+          />
+          <Painting
+            map='https://res.cloudinary.com/dexin8o58/image/upload/v1685221048/three_j414x5.gif'
+            position={[-2.95, 0, -3]}
+          />
+        </group>
       </Canvas>
     </>
   )
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react"
 import { useTexture } from "@react-three/drei"
 
 export function Painting({
   position = [0, 0, 0],
   scale = [5, 3, 15],
+  color = "white",
   thickness = 0.01,
   map = "https://res.cloudinary.com/dexin8o58/image/upload/v1676239383/5B758959-5682-4A83-AA20-A429C53AF854_exp9da.jpg",
 }) {
+  const [hovered, setHover] = useState(false)
   const texture = useTexture({
     map: map,
   })
   return (
-    <mesh castShadow position={position}>
+    <mesh onPointerOver={() => setHover(true)} onPointerOut={() => setHover(false)} position={position}>
       <boxGeometry args={[thickness, 1.5, 1.5]} />
-      <meshStandardMaterial {...texture} color='hotpink' />
+      <meshStandardMaterial {...texture} color={color} />
     </mesh>
   )
 }
