5532639a281093df478896402e4d4a25ca64ed18
Santista1 <santista1@proton.me>
2023-06-05 11:51:09 -0300

    -small changes

--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -3,10 +3,10 @@ export function Sphere({ position, color = 0xffd83d }) {
     <>
       <group position={position}>
         {/* rotation={[0, 90 * (Math.PI / 180), 16 * (Math.PI / 180)]} */}
-        <mesh castShadow>
+        {/* <mesh castShadow>
           <sphereGeometry args={[25, 32, 2, 0, Math.PI * 2, 0, Math.PI * 0.2]} />
           <meshPhongMaterial side={2} color={color} />
-        </mesh>
+        </mesh> */}
         <mesh castShadow rotation={[0, 0, 0]}>
           <sphereGeometry args={[25, 32, 2, 0, Math.PI * 2, Math.PI * 0.33, Math.PI * 0.15]} />
           <meshPhongMaterial side={2} color={color} />
