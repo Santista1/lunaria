2f0b700a4a9f81bdc9683a6429ee8f33b3449fea
Santista1 <santista1@proton.me>
2023-06-09 22:22:25 -0300

    -light switches edit

--- src/components/Hallway.jsx
+++ src/components/Hallway.jsx
@@ -3,14 +3,6 @@ import { Instances, Instance } from "@react-three/drei"
 import { useState } from "react"
 
 export function Hallway() {
-  const [switch0, setSwitch0] = useState(true)
-  const [on1, setOn1] = useState(true)
-  const [on2, setOn2] = useState(true)
-  const [on3, setOn3] = useState(true)
-  const [on4, setOn4] = useState(true)
-  const [on5, setOn5] = useState(true)
-  const [on6, setOn6] = useState(true)
-
   return (
     <>
       <Instances>
@@ -34,69 +26,83 @@ export function Hallway() {
         <Columns position={[0, 0, -75]} />
         <Columns position={[0, 0, -90]} />
       </Instances>
-      <pointLight castShadow distance={10} color='blue' position={[0, 0, 0]} intensity={switch0 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='hotpink' position={[0, 0, -15]} intensity={on1 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='blue' position={[0, 0, -30]} intensity={on2 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='hotpink' position={[0, 0, -45]} intensity={on3 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='blue' position={[0, 0, -60]} intensity={on4 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='hotpink' position={[0, 0, -75]} intensity={on5 ? 0.4 : 0} />
-      <pointLight castShadow distance={10} color='blue' position={[0, 0, -90]} intensity={on6 ? 0.4 : 0} />
+      <Lights />
+    </>
+  )
+}
+
+function Lights() {
+  const [switch0, setSwitch0] = useState(true)
+  const [on1, setOn1] = useState(true)
+  const [on2, setOn2] = useState(true)
+  const [on3, setOn3] = useState(true)
+  const [on4, setOn4] = useState(true)
+  const [on5, setOn5] = useState(true)
+  const [on6, setOn6] = useState(true)
+
+  const coverScale = [0.03, 0.12, 0.08]
+  const buttonScale = [0.05, 0.03, 0.03]
+
+  return (
+    <>
+      <pointLight castShadow distance={12} color='blue' position={[0, 0, 0]} intensity={switch0 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='hotpink' position={[0, 0, -15]} intensity={on1 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='blue' position={[0, 0, -30]} intensity={on2 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='hotpink' position={[0, 0, -45]} intensity={on3 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='blue' position={[0, 0, -60]} intensity={on4 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='hotpink' position={[0, 0, -75]} intensity={on5 ? 0.4 : 0} />
+      <pointLight castShadow distance={12} color='blue' position={[0, 0, -90]} intensity={on6 ? 0.4 : 0} />
       <Instances>
         <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial shininess={0} />
-        <Instance
-          onClick={() => setSwitch0(!switch0)}
-          color='red'
-          position={[2.9, 0, 4.3]}
-          scale={[0.04, 0.04, 0.04]}
-        />
+        <meshBasicMaterial />
+        <Instance onClick={() => setSwitch0(!switch0)} color='red' position={[2.9, 0, 4.3]} scale={buttonScale} />
         <Instance
           onClick={() => setSwitch0(!switch0)}
           color={switch0 ? "white" : "black"}
           position={[2.9, 0, 4.3]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn1(!on1)} color='red' position={[2.9, 0, 4.3 - 15]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn1(!on1)} color='red' position={[2.9, 0, 4.3 - 15]} scale={buttonScale} />
         <Instance
           onClick={() => setOn1(!on1)}
           color={on1 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 15]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn2(!on2)} color='red' position={[2.9, 0, 4.3 - 30]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn2(!on2)} color='red' position={[2.9, 0, 4.3 - 30]} scale={buttonScale} />
         <Instance
           onClick={() => setOn2(!on2)}
           color={on2 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 30]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn3(!on3)} color='red' position={[2.9, 0, 4.3 - 45]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn3(!on3)} color='red' position={[2.9, 0, 4.3 - 45]} scale={buttonScale} />
         <Instance
           onClick={() => setOn3(!on3)}
           color={on3 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 45]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn4(!on4)} color='red' position={[2.9, 0, 4.3 - 60]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn4(!on4)} color='red' position={[2.9, 0, 4.3 - 60]} scale={buttonScale} />
         <Instance
           onClick={() => setOn4(!on4)}
           color={on4 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 60]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn5(!on5)} color='red' position={[2.9, 0, 4.3 - 75]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn5(!on5)} color='red' position={[2.9, 0, 4.3 - 75]} scale={buttonScale} />
         <Instance
           onClick={() => setOn5(!on5)}
           color={on5 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 75]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
-        <Instance onClick={() => setOn6(!on6)} color='red' position={[2.9, 0, 4.3 - 90]} scale={[0.04, 0.04, 0.04]} />
+        <Instance onClick={() => setOn6(!on6)} color='red' position={[2.9, 0, 4.3 - 90]} scale={buttonScale} />
         <Instance
           onClick={() => setOn6(!on6)}
           color={on6 ? "white" : "black"}
           position={[2.9, 0, 4.3 - 90]}
-          scale={[0.03, 0.065, 0.065]}
+          scale={coverScale}
         />
       </Instances>
     </>
--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -4,9 +4,8 @@ import { useThree } from "@react-three/fiber"
 
 export function Poster({
   position = [0, 0, 0],
-  scale = [0.01, 1.5, 1.5],
+  scale = [1.5, 1.5],
   color = "white",
-  thickness = 0.01,
   map = "https://res.cloudinary.com/dexin8o58/image/upload/v1676239383/5B758959-5682-4A83-AA20-A429C53AF854_exp9da.jpg",
   owner = "-",
 }) {
@@ -24,7 +23,7 @@ export function Poster({
         font={"./JuraBook.ttf"}
         fontSize={0.1}
         color='yellow'
-        visible={hovered}
+        fillOpacity={hovered ? 1 : 0}
       >
         {"owner: " + owner}
       </Text>
@@ -33,7 +32,7 @@ export function Poster({
         onClick={() => {
           camera.position.set(0, 0, 0)
         }}
-        args={[1.5, 1.5]}
+        args={scale}
         rotation={[0, 90 * (Math.PI / 180), 0]}
         onPointerOver={() => setHover(true)}
         onPointerOut={() => setHover(false)}
