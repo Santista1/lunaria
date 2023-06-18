a641318584851de0ea76341496dc252d769d1020
Santista1 <santista1@proton.me>
2023-06-18 04:09:35 -0300

    -css animation

--- src/Html.jsx
+++ src/Html.jsx
@@ -5,52 +5,57 @@ export default function Images() {
   return (
     <>
       <group position={[-x, 0, 0]}>
+        <Html position={[0, 1.5, 0]} rotation={[0, 90 * (Math.PI / 180), 0]} occlude transform pointerEvents='none'>
+          <div class='marquee'>
+            <p>Lunc Monkeys</p>
+          </div>
+        </Html>
         <Image
           pos={[0, 0, 3]}
           rotation={[0, 90 * (Math.PI / 180), 0]}
           src='https://res.cloudinary.com/dexin8o58/image/upload/v1676239383/5B758959-5682-4A83-AA20-A429C53AF854_exp9da.avif'
         />
-        <Html position={[0, -0.9, 3]} rotation={[0, 90 * (Math.PI / 180), 0]} occlude transform pointerEvents='none'>
-          <p
-            style={{
-              fontSize: "5px",
-              color: "#fdf29f",
-            }}
-          >
-            Owner: Price:
-          </p>
-        </Html>
+        <Text pos={[0, -0.9, 3]} rot={[0, 90 * (Math.PI / 180), 0]} text='Owner: OG Price: -' />
         <Image
           pos={[0, 0, 1]}
           rotation={[0, 90 * (Math.PI / 180), 0]}
           src='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/IMG_5638_vqwa7h.avif'
         />
+        <Text pos={[0, -0.9, 1]} rot={[0, 90 * (Math.PI / 180), 0]} text='Owner: OG Price: -' />
+
         <Image
           pos={[0, 0, -1]}
           rotation={[0, 90 * (Math.PI / 180), 0]}
           src='https://res.cloudinary.com/dexin8o58/image/upload/v1676239389/IMG_5667_fhvrbt.avif'
         />
+        <Text pos={[0, -0.9, -1]} rot={[0, 90 * (Math.PI / 180), 0]} text='Owner: OG Price: -' />
         <Image
           pos={[0, 0, -3]}
           rotation={[0, 90 * (Math.PI / 180), 0]}
           src='https://res.cloudinary.com/dexin8o58/image/upload/v1676239388/AACABEAC-F96C-46D9-8AC5-83C5994C7F4E_grnioz.avif'
         />
+        <Text pos={[0, -0.9, -3]} rot={[0, 90 * (Math.PI / 180), 0]} text='Owner: OG Price: -' />
       </group>
       <group position={[x, 0, 0]}>
+        <Html position={[0, 1.5, 0]} rotation={[0, 270 * (Math.PI / 180), 0]} occlude transform pointerEvents='none'>
+          <div class='marquee'>
+            <p>Lunc Monkeys</p>
+          </div>
+        </Html>
         <Image
           pos={[0, 0, 3]}
           rotation={[0, 270 * (Math.PI / 180), 0]}
-          src='https://res.cloudinary.com/dexin8o58/image/upload/v16862703135/Screenshot_20230608-155159_Twitter_ww309n.avif'
+          src='https://res.cloudinary.com/dexin8o58/image/upload/v16862703135/Nfts/Screenshot_20230608-155159_Twitter_ww309n.avif'
         />
         <Image
           pos={[0, 0, 1]}
           rotation={[0, 270 * (Math.PI / 180), 0]}
-          src='https://res.cloudinary.com/dexin8o58/image/upload/v16862703268/FyGetU5XsAILt4b_k1jmz9.avif'
+          src='https://res.cloudinary.com/dexin8o58/image/upload/v16862703268/Nfts/FyGetU5XsAILt4b_k1jmz9.avif'
         />
         <Image
           pos={[0, 0, -1]}
           rotation={[0, 270 * (Math.PI / 180), 0]}
-          src='https://res.cloudinary.com/dexin8o58/image/upload/v16862703271/FyGqUGBagAAON3q_vyfxht.avif'
+          src='https://res.cloudinary.com/dexin8o58/image/upload/v1686903271/Nfts/FyGqUGBagAAON3q_vyfxht.avif'
         />
         <Image
           pos={[0, 0, -3]}
@@ -134,5 +139,20 @@ function Image({ src, pos, rotation }) {
         />
       </Html>
     </>
+  )
+}
+
+function Text({ text, pos, rot }) {
+  return (
+    <Html position={pos} rotation={rot} occlude transform pointerEvents='none'>
+      <p
+        style={{
+          fontSize: "5px",
+          color: "#fdf29f",
+        }}
+      >
+        {text}
+      </p>
+    </Html>
   )
 }
